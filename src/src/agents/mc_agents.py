232d5dc368b54e66ddea3e0da6e3e794a9a180c2
Eric Bauer <eric.bauer55@gmail.com>
2025-02-02 14:41:22 -0500

    added more classes to handle timesteps of SAR and trajectories to contain them

--- src/src/agents/mc_agents.py
+++ src/src/agents/mc_agents.py
@@ -1,13 +1,15 @@
 import numpy as np
 from collections import defaultdict
 import pandas as pd
-from typing import Optional, List
+from typing import Optional, List, TypeVar
 from functools import reduce
+from dataclasses import dataclass, field
 
 class Policy:
-    def __init__(self):
+    def __init__(self, epsilon:float=0.01):
         # outer key = state, inner key = action, value = probability pi(s,a)
         self.policy = defaultdict(defaultdict(lambda x: 0.0))
+        self.epsilon = epsilon # probability to sample off-policy in a uniform way
     
     def sample_action(self, state:str, valid_actions:Optional[List[str]])->str:
         '''
@@ -27,18 +29,65 @@ class Policy:
                 for a in actions: # use the action_space so far to set a uniform action distribution for state
                     self.policy[state][a] = 1.0 / len(actions) 
 
-            
+        # Filter which actions will be sampled from and which ones will have 0.0 probability
         if valid_actions is not None:
             actions_mask = [True if a in valid_actions else False for a in actions]
         else:
             actions_mask = [True] * len(actions)
+        
+        # Determine the action sampling probabilities
+        # Check epsilon exploration (which will provide uniform action sampling)
+        if np.random.choice(['off-policy','greedy on-policy'], p=[self.epsilon, 1-self.epsilon])=='off-policy':
+            action_prob = [1.0/sum(actions_mask) if masked else 0.0 for masked in actions_mask]
+        else:
+            action_prob = [self.policy[state][a] if masked else 0.0 for a, masked in zip(actions, actions_mask)]
+            # normalize the action probabilities so sum(valid_actions_probs) = 1.0
+            action_prob = [p/sum(action_prob) for p in action_prob]
+        
+        # Sample the action
+        sampled_action = np.random.choice(actions, p=action_prob)
+        return sampled_action
+
 
     def _union_action_space(self) -> List[str]:
         actions_by_state = [set(self.policy[state].keys()) for state in self.policy.keys()]
         all_actions = reduce(set.union, actions_by_state)
         return all_actions
 
+    def renormalize_action_space(self) -> None:
+        '''
+        This method guarantees that within each state of the policy, the action probabilities sum to 1.0
+        This is relevant as different actions are added to the self.policy[state] dictionary
+        '''
+        for state in self.policy.keys():
+            action_probs = [p for p in self.policy[state].values()]
+            for action in self.policy[state].keys():
+                self.policy[state][action] = self.policy[state][action] / sum(action_probs)
+
+S = TypeVar('S', str, dict, int)
+A = TypeVar('A', str, int)
+R = TypeVar('R', int, float)
+
+@dataclass
+class Timestep:
+    state: S
+    action: A
+    reward: R
+
+class Trajectory:
+    def __init__(self):
+        self.trajectory: List[Timestep] = []
+    
+    def append(self, timestep_sar: Timestep):
+        self.trajectory.append(timestep_sar)
+
 
 class Agent:
-    def __init__(self):
-        self.pi = defaultdict(defaultdict(lambda x: 0.0))
+    def __init__(self, env, epsilon=0.01):
+        self.policy = Policy(epsilon=epsilon)
+        self.env = env
+        self.quality = defaultdict(defaultdict(lambda x:0.0))
+        self.episode_log: List[Trajectory] = []
+
+    def play_episodes(self, m_episodes:int=100):
+        pass
